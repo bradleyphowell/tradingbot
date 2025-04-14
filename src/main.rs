3a7e05f64ca9761f0c66c19f595a91b48c8ddd28
bradleyphowell <bradleyphowell@gmail.com>
2025-04-14 09:02:22 +0100

    Created bidness function and created driver variables

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,8 @@ fn main() {
 }
 
 \********************************************************************************************/
+
+/*********************************Testing the inventory skew**********************************\
     let mut base_assets = 100.0;
     let mut quote_assets = 10000.0;
 
@@ -51,12 +53,26 @@ fn main() {
         price: 100.0,
     };
 
-    let (mut base_assets, mut quote_assets) = inventory::inventory_balance(base_assets, quote_assets, &trade);
 
+    //Calculates inventory skew as a linear relationship to exposure with 0 representing max long  
+    //exposure so bots should offered and 1 representing max short exposure so bots should be bid.
+    let (mut base_assets, mut quote_assets) = inventory::inventory_balance(base_assets, quote_assets, &trade);
+    
     let x: f64 = (config::START_BASE_INVENTORY - base_assets)/(config::RISK_LIMIT * 2.0);
     let inventory_skew = 0.5 + x;  
     println!("Base assets: {}", base_assets);
     println!("Quote assets: {}", quote_assets);
     println!("Inventory skew: {}", inventory_skew);  
+
+\********************************************************************************************/
+
+
+
+}
+
+fn bidness_calc(inventory_skew, ob_vol_weight, of_balance, A, B, C) -> f64 {
+
+    let mut bidness = 0.0;
+    
 
 }
